2500a9b17a7f83a91825b45f8819c38e60c17dcc
Claudio Popolin <claudio.junior@beblue.com.br>
2018-04-30 14:24:36 -0300

    Primeira tela feita

--- app/src/main/java/cartaovirtual/prj/ifsp/com/cartaovirtual/MainActivity.java
+++ app/src/main/java/cartaovirtual/prj/ifsp/com/cartaovirtual/MainActivity.java
@@ -2,12 +2,48 @@ package cartaovirtual.prj.ifsp.com.cartaovirtual;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tollbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            /*case R.id.search_menu:{
+                Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+                break;
+            }*/
+            case R.id.myCards_menu:{
+                Toast.makeText(this, "Vai para Activity dos Cartões", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.about_menu:{
+                Toast.makeText(this, "Vai par tela que fala sobre aplicativo", Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/cartaovirtual/prj/ifsp/com/cartaovirtual/MeusCartoesActivity.java
@@ -0,0 +1,13 @@
+package cartaovirtual.prj.ifsp.com.cartaovirtual;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeusCartoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_cartoes);
+    }
+}
